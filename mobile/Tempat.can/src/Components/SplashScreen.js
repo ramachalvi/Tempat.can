44f0c814555ffd7be53338b8a4bc056180a6f015
hasimy-as <ahmadhasyimi674@gmail.com>
2020-02-19 22:54:11 +0700

    added splashscreen

--- mobile/Tempat.can/src/Components/SplashScreen.js
+++ mobile/Tempat.can/src/Components/SplashScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Image } from 'react-native';
+import { View, Text, Image } from 'react-native';
 
 export default class SplashScreen extends Component {
   render() {
@@ -13,12 +13,15 @@ export default class SplashScreen extends Component {
         justifyContent: 'center',
         backgroundColor: '#121212'
       }}>
-        <Image
+        {/* <Image
           source={{
-            uri: 'https://raw.githubusercontent.com/hasimy-as/Tempat.can/blob/develop/mobile/Tempat.can/src/Assets/tempatcan.png'
+            uri: 'https://github.com/hasimy-as/Tempat.can/blob/develop/mobile/Tempat.can/src/Assets/tempatcan.png?raw=true'
           }}
           style={{ width: 100, height: 100 }}
-        />
+        /> */}
+        <Text style={{color: 'white', fontSize: 44}}>
+          Tempatcan kuy!
+        </Text>
         </View>
     );
   }
